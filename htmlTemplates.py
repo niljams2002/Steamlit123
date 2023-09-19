16a8964e53c83ba29bd38948ab0590f7135b8ce9
NILESH RAVICHANDRAN <86390311+niljams2002@users.noreply.github.com>
2023-09-19 16:56:24 +0530

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://static.vecteezy.com/system/resources/previews/007/742/912/non_2x/circle-green-dots-isolated-on-the-black-background-geometric-art-design-element-for-frame-logo-tattoo-web-pages-prints-posters-template-abstract-backgrounds-optical-illusion-shape-vector.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://global.discourse-cdn.com/business5/uploads/expo/original/3X/4/6/46bedbd705a0872a19064f7e58b7904633031a3e.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
