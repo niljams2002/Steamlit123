80d258a545456c08d9f3b38d20e23323dea445e2
NILESH RAVICHANDRAN <86390311+niljams2002@users.noreply.github.com>
2023-09-20 13:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 import os
 ## from langchain.llms import HuggingFaceHub
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
