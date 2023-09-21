33f92cc06c67ce79eef069d2b4938c328939cd18
NILESH RAVICHANDRAN <86390311+niljams2002@users.noreply.github.com>
2023-09-21 18:22:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,10 @@ def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
+    # Reverse the order of chat history to display the most recent message at the top
+    reversed_chat_history = reversed(st.session_state.chat_history)
+
+    for i, message in enumerate(reversed_chat_history):
         if i % 2 == 0:
             st.write(user_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
